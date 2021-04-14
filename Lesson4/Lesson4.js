8fb998daba33e416c508e8c2d3e55b8d9a46f8c8
irynadobr <irenkad@ukr.net>
2021-04-14 16:50:48 +0300

    lesson 4 - OOP

--- Lesson4/Lesson4.js
+++ Lesson4/Lesson4.js
@@ -1,8 +1,7 @@
 // 1) створити функцію яка приймає масив та виводить його
 
 function arrays(arr) {
-    let array = arr;
-    console.log(array);
+    console.log(arr);
 }
 
 arrays([1, 2, 3]);
@@ -59,14 +58,11 @@ max(520, 2, 20);
 // 5) створити функцію яка повертає найбільше число з масиву
 
 function maxNumberOfArray(arrays) {
-    let arr = arrays;
-    let max =arr[0];
-
-    for (let i = 1; i < arr.length; i++) {
-        const arrElement = arr[i];
-
-        if (arr[i]>max) {
-            max=arr[i]
+    let max =arrays[0];
+    for (let i = 1; i < arrays.length; i++) {
+        const arrElement = arrays[i];
+        if (arrays[i]>max) {
+            max=arrays[i]
         }
     }
     return max;
@@ -77,14 +73,11 @@ console.log(maxNumberOfArray([1, 500, 700, 4, 20000, 10, 2000, 1000]));
 // 6) створити функцію яка повертає найменьше число з масиву
 
 function minNumberOfArray(arrays) {
-    let arr = arrays;
-    let min =arr[0];
-
-    for (let i = 1; i < arr.length; i++) {
-        const arrElement = arr[i];
-
-        if (arr[i]<min) {
-            min=arr[i]
+    let min =arrays[0];
+    for (let i = 1; i < arrays.length; i++) {
+        const arrElement = arrays[i];
+        if (arrays[i]<min) {
+            min=arrays[i]
         }
     }
     return min;
@@ -95,11 +88,10 @@ console.log(minNumberOfArray([1, 500, 700, 0, 20000, 10, 2000, 1000]));
 // 7) створити функцію яка приймає масив чисел, сумує значення елементів масиву та повертає його.
 
 function arraysOfNumber(arraysNumber) {
-    let arr = arraysNumber;
     let sum = 0;
-    for (let i = 0; i < arr.length; i++) {
-        const arrElement = arr[i];
-        sum += arr[i];
+    for (let i = 0; i < arraysNumber.length; i++) {
+        const arrElement = arraysNumber[i];
+        sum += arraysNumber[i];
     }
     return sum;
 }
@@ -109,13 +101,12 @@ console.log(arraysOfNumber([1, 500, 20, 4, 7, 15]));
 
 // 8) створити функцію яка приймає масив чисел та повертає середнє арифметичне його значень.
 function arrOfNumber(arrNumber) {
-    let arr = arrNumber;
     let sum = 0;
-    for (let i = 0; i < arr.length; i++) {
-        const arrElement = arr[i];
-        sum += arr[i];
+    for (let i = 0; i < arrNumber.length; i++) {
+        const arrElement = arrNumber[i];
+        sum += arrNumber[i];
     }
-    return sum / arr.length;
+    return sum / arrNumber.length;
 }
 
 console.log(arrOfNumber([10, 5, 80, 100]));
@@ -125,9 +116,8 @@ console.log(arrOfNumber([10, 5, 80, 100]));
 //     [{name: 'Dima', age: 13}, {model: 'Camry'}]  ===> [ name, age, model ]
 
 function arrObject(arrObj) {
-    let arr = arrObj;
     let arrKeys = []
-    for (let value of arr) {
+    for (let value of arrObj) {
         for (let key in value) {
             arrKeys.push(key);
         }
@@ -141,11 +131,10 @@ console.log(arrObject([{name: 'Dima', age: 13}, {model: 'Camry'}]));
 //або 2-й варіант
 
 function arrKeys(arrObj) {
-    let arr = arrObj;
     let arrKeys = []
-    for (let i = 0; i < arr.length; i++) {
-        const arrElement = arr[i];
-        let objectKeys = Object.keys(arrElement);
+    for (let i = 0; i < arrObj.length; i++) {
+        const arrElement = arrObj[i];
+        let objectKeys = arrObj.keys(arrElement);
         for (let value of objectKeys) {
             arrKeys.push(value);
         }
@@ -160,10 +149,9 @@ console.log(arrKeys([{name: 'Dima', age: 13}, {model: 'Camry'}]));
 //     [{name: 'Dima', age: 13}, {model: 'Camry'}]  ===> [ Dima, 13, Camry ]
 
 function arraysObject(arrAnyObj) {
-    let arr = arrAnyObj;
     let arrValues = []
-    for (let i = 0; i < arr.length; i++) {
-        const arrElement = arr[i];
+    for (let i = 0; i < arrAnyObj.length; i++) {
+        const arrElement = arrAnyObj[i];
         let objectVelues = Object.values(arrElement);
         for (let value of objectVelues) {
             arrValues.push(value);
@@ -182,15 +170,13 @@ console.log(arraysObject([{name: 'Dima', age: 13}, {model: 'Camry'}]));
 //     [3,5,7,9]
 
 function sumElementOfArrays(array1, array2) {
-    let arr1 = array1;
-    let arr2 = array2;
     let arrSumElementsOfArrays = [];
-    for (let i = 0; i < arr1.length; i++) {
-        const arr1Element = arr1[i];
-        for (let j = 0; j < arr2.length; j++) {
-            const arr2Element = arr2[j];
+    for (let i = 0; i < array1.length; i++) {
+        const arr1Element = array1[i];
+        for (let j = 0; j < array2.length; j++) {
+            const arr2Element = array2[j];
             if (i === j) {
-                let sumElementOfArrays = arr1[i] + arr2[j];
+                let sumElementOfArrays = array1[i] + array2[j];
                 arrSumElementsOfArrays.push(sumElementOfArrays);
             }
         }
@@ -205,9 +191,8 @@ console.log(sumElementOfArrays([1, 2, 3, 4], [2, 3, 4, 5]));
 // - створити функцію яка приймає будь-яку кількість чисел, повертає найменьше,
 // а виводить найбільше (Math використовувати заборонено);
 function minMaxOfAnyAmountOfNambers(anyAmountOfNambers) {
-    let amountOfNambers = anyAmountOfNambers;
     let arrNumbers = []
-    for (let i = 0; i < amountOfNambers; i++) {
+    for (let i = 0; i < anyAmountOfNambers; i++) {
         let number = prompt(`enter a number ${i + 1}`);
         console.log(number);
         arrNumbers.push(number);
@@ -216,7 +201,7 @@ function minMaxOfAnyAmountOfNambers(anyAmountOfNambers) {
             return a - b;
         }
     )
-    console.log('amountOfNamber = ' + amountOfNambers);
+    console.log('amountOfNamber = ' + anyAmountOfNambers);
     console.log(' maxOfAnyAmountOfNambers = ' + arrNumbers[arrNumbers.length - 1]);
     return 'minOfAnyAmountOfNambers = ' + arrNumbers[0];
 }
@@ -245,14 +230,13 @@ console.log(AnyAmountOfNambers(1, 50, 2, 3, 100, 600, 8, 900, 550));
 //     Що б я міг сам вибрати повернути мені масив ключів чи масив значень.
 
 function returnValueOrKays(arrAnyObj) {
-    let arr = arrAnyObj;
     let arrValueOrKays = [];
     let returnValueOrKays = prompt('Для того, щоб повернути масив ключів, виберіть - 1, Для того, щоб повернути масив значень, виберіть - 0');
     console.log(returnValueOrKays);
     switch (returnValueOrKays) {
         case '0':
-            for (let i = 0; i < arr.length; i++) {
-                const arrElement = arr[i];
+            for (let i = 0; i < arrAnyObj.length; i++) {
+                const arrElement = arrAnyObj[i];
                 let objectValues = Object.values(arrElement);
 
                 for (let value of objectValues) {
@@ -261,7 +245,7 @@ function returnValueOrKays(arrAnyObj) {
             }
             return arrValueOrKays;
         case '1':
-            for (let value of arr) {
+            for (let value of arrAnyObj) {
                 for (let kay in value) {
                     arrValueOrKays.push(kay);
                 }
@@ -282,33 +266,29 @@ console.log(returnValueOrKays([{name: 'Dima', age: 13}, {model: 'Camry'}]));
 //   foo([9,8,0,4], 2) // ==> [ 9, 8, 4, 0 ]
 
 function objectIndex(arr, index) {
-    let arrays = arr;
-    let ind = index;
-    let a1 = arrays[ind];
-    let a2 = arrays[ind+1];
-    arrays[ind] =a2;
-    arrays[ind+1] = a1;
-    console.log(arrays);
+    let a1 = arr[index];
+    let a2 = arr[index+1];
+    arr[index] =a2;
+    arr[index+1] = a1;
+    console.log(arr);
 }
 
 objectIndex([9, 8, 0, 4], 0);
 objectIndex([9, 8, 0, 4], 1);
 objectIndex([9, 8, 0, 4], 2);
 
-//function objectIndex(arr, index) {
-//     let arrays = arr;
-//     let ind = index;
+// function objectIndex(arr, index) {
 //     let rezultat = [];
 //     let newArray = [];
-//     newArray.push(arrays[ind + 1]);
-//     newArray.push(arrays[ind]);
-//     for (let i = 0; i < arrays.length; i++) {
-//         if (i === ind) {
+//     newArray.push(arr[index + 1]);
+//     newArray.push(arr[index]);
+//     for (let i = 0; i < arr.length; i++) {
+//         if (i === index) {
 //             rezultat.push(newArray[0]);
 //             rezultat.push(newArray[1]);
 //             i = i + 1;
 //         } else {
-//             rezultat.push(arrays[i])
+//             rezultat.push(arr[i])
 //         }
 //     }
 //     console.log(rezultat);
@@ -326,15 +306,14 @@ objectIndex([9, 8, 0, 4], 2);
 // [0,1,2,3,4] => [1,2,3,4,0]
 // [0,0,1,0]   => [1,0,0,0]
 function newArray(arr) {
-    let array = arr;
-    for (let i = array.length - 2; i >= 0; i--) {
-        const arrayElement = array[i];
+    for (let i = arr.length - 2; i >= 0; i--) {
+        const arrayElement = arr[i];
         if (arrayElement === 0) {
-            array.splice(i, 1);
-            array.push(arrayElement);
+            arr.splice(i, 1);
+            arr.push(arrayElement);
         }
     }
-    console.log(array);
+    console.log(arr);
 }
 
 newArray([1, 0, 6, 0, 3]);
@@ -351,8 +330,7 @@ newArray([0, 0, 1, 0]);
 // let n3 = 'Hermione Granger'
 
 function normalizesTheName(nameString) {
-    let name = nameString;
-    let array = name.trim().split(' ')
+    let array = nameString.trim().split(' ')
     let newArray = [];
     newArray.push(array[0]);
     newArray.push(array[array.length - 1]);
@@ -391,11 +369,10 @@ addContent('p', 'Додає в боді елемент з текстом');
 // Всі властивості авто в обному блоці
 
 function arrCar(typeElement, car) {
-    let cars = car;
     let element = document.createElement(typeElement);
     document.body.appendChild(element);
-    for (let i = 0; i < cars.length; i++) {
-        const carElement = cars[i];
+    for (let i = 0; i < car.length; i++) {
+        const carElement = car[i];
         let blokCar = document.createElement(typeElement);
         for (let j = 0; j < Object.keys(carElement).length; j++) {
             blokCar.innerText += ' ' + (Object.keys(carElement))[j] + ': ' + (Object.values(carElement))[j] + ',';
@@ -416,11 +393,10 @@ arrCar('div', [{model: 'lada', color: 'green', age: 2000}, {model: 'BMW', color:
 // Для кожної властивості створити всередені блока автомоблія свій блок
 // // ******************* НАЗАД В МИНУЛЕ *********************** //
 function arrayCar(typeElement, car) {
-    let cars = car;
     let element = document.createElement(typeElement);
     document.body.appendChild(element);
-    for (let i = 0; i < cars.length; i++) {
-        const carElement = cars[i];
+    for (let i = 0; i < car.length; i++) {
+        const carElement = car[i];
         let blokCar = document.createElement(typeElement);
         for (let j = 0; j < Object.keys(carElement).length; j++) {
             let blocProperty = document.createElement(typeElement);
