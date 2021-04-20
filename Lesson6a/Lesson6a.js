cc6b3b8ca8da8166fc22b0f5640285ea786b0a70
irynadobr <irenkad@ukr.net>
2021-04-20 23:27:24 +0300

    Lesson6a - array methods-work in the audience

--- Lesson6a/Lesson6a.js
+++ Lesson6a/Lesson6a.js
@@ -3,100 +3,19 @@
 
 
 const cars = [
-    {
-        producer: "subaru",
-        model: "wrx",
-        year: 2010,
-        color: "blue",
-        type: "sedan",
-        engine: "ej204x",
-        volume: 2,
-        power: 400
-    },
-    {
-        producer: "subaru",
-        model: "legacy",
-        year: 2007,
-        color: "silver",
-        type: "sedan",
-        engine: "ez30",
-        volume: 3,
-        power: 250
-    },
-    {
-        producer: "subaru",
-        model: "tribeca",
-        year: 2011,
-        color: "white",
-        type: "jeep",
-        engine: "ej20",
-        volume: 2,
-        power: 300
-    },
-    {
-        producer: "subaru",
-        model: "leone",
-        year: 1998,
-        color: "yellow",
-        type: "sedan",
-        engine: "ez20x",
-        volume: 2,
-        power: 140
-    },
-    {
-        producer: "subaru",
-        model: "impreza",
-        year: 2014,
-        color: "red",
-        type: "sedan",
-        engine: "ej204x",
-        volume: 2,
-        power: 200
-    },
-    {
-        producer: "subaru",
-        model: "outback",
-        year: 2014,
-        color: "red",
-        type: "hachback",
-        engine: "ej204",
-        volume: 2,
-        power: 165
-    },
+    { producer: "subaru", model: "wrx",year: 2010, color: "blue", type: "sedan", engine: "ej204x", volume: 2, power: 400},
+    { producer: "subaru", model: "legacy", year: 2007, color: "silver", type: "sedan", engine: "ez30", volume: 3, power: 250},
+    { producer: "subaru", model: "tribeca", year: 2011, color: "white", type: "jeep", engine: "ej20", volume: 2, power: 300},
+    { producer: "subaru", model: "leone", year: 1998, color: "yellow", type: "sedan", engine: "ez20x", volume: 2, power: 140},
+    { producer: "subaru", model: "impreza", year: 2014, color: "red", type: "sedan", engine: "ej204x", volume: 2, power: 200},
+    { producer: "subaru", model: "outback", year: 2014, color: "red", type: "hachback", engine: "ej204", volume: 2, power: 165},
     {producer: "bmw", model: "115", year: 2013, color: "red", type: "hachback", engine: "f15", volume: 1.5, power: 120},
     {producer: "bmw", model: "315", year: 2010, color: "white", type: "sedan", engine: "f15", volume: 1.5, power: 120},
     {producer: "bmw", model: "650", year: 2009, color: "black", type: "coupe", engine: "f60", volume: 6, power: 350},
     {producer: "bmw", model: "320", year: 2012, color: "red", type: "sedan", engine: "f20", volume: 2, power: 180},
-    {
-        producer: "mercedes",
-        model: "e200",
-        year: 1990,
-        color: "silver",
-        type: "sedan",
-        engine: "eng200",
-        volume: 2,
-        power: 180
-    },
-    {
-        producer: "mercedes",
-        model: "e63",
-        year: 2017,
-        color: "yellow",
-        type: "sedan",
-        engine: "amg63",
-        volume: 3,
-        power: 400
-    },
-    {
-        producer: "mercedes",
-        model: "c250",
-        year: 2017,
-        color: "red",
-        type: "sedan",
-        engine: "eng25",
-        volume: 2.5,
-        power: 230
-    }
+    {producer: "mercedes", model: "e200", year: 1990, color: "silver", type: "sedan", engine: "eng200", volume: 2, power: 180},
+    {producer: "mercedes", model: "e63", year: 2017, color: "yellow", type: "sedan", engine: "amg63", volume: 3, power: 400},
+    {producer: "mercedes", model: "c250", year: 2017, color: "red", type: "sedan", engine: "eng25", volume: 2.5, power: 230}
 ];
 
 // Відфільтрувати масив за наступними крітеріями :
@@ -304,12 +223,6 @@ console.log(userIsMarriedChild);
 // Імя, вік, стаж водіння.
 //     Створити не менше 7 та не більше 20 машинок.
 //     Для початку вкладіть всі наші створені автомобілі в масив cars.
-//     Зробити половину автопарку ремонт мотору, що збільшить потужність автомобілів на 10% (переприсвоєння змінної потужності).
-// На відремонтовані автомобілі найняти нових водіїв (переприсвоїти змінну водій).
-// Далі необхідно брати кожну другу машинку (цикл з кроком в 2), та робити їй підвищення потужності двигуна на 10% та ціну на 5%
-// Після того зробити перевірку досвіду ВСІХ наших водіїв. Якщо досвід водія менший за 5 років, але його вік більший за 25,
-//     то необідно відправити його на курси підвищення кваліфікації, що збільшить йому досвід на 1 рік.
-//     Також спробуйте порахувати суму, яку потрібно потратити для покупки всіх цих авто в циклі
 
 class Car {
     constructor(model, power, driverCar, price, age) {
